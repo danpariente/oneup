83b74a1854ad00f5fc19342b10727f55532ca96a
naughtystyle <cute.dan@gmail.com>
2012-08-07 04:46:22 -0400

    lil tweaks

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,6 @@ class Activity < ActiveRecord::Base
   has_many :comments, :as => :commentable	
   has_many :likes, :as => :likeable
 
-  scope :applications_feed, where(activity_type: 'application').order('created_at DESC')  
+  scope :applications_feed, where(activity_type: 'application').order('created_at DESC').limit(10)  
   scope :recent, order('created_at DESC').limit(5)
 end
